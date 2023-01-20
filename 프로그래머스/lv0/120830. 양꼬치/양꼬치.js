5eba2c230d85c978dca755c6eaa38b569e7db574
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-21 03:27:47 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -0,0 +1,10 @@
+//    function solution(n, k) {
+//          // return n * 12000 + k * 2000 - parseInt(n / 10) * 2000;
+//          // return n * 12000 + k * 2000 - Math.floor(n / 10) * 2000;
+     
+//                                     //이것이 틸트 연산자
+//          return 
+//    }
+
+
+  let solution = (n, k) => n * 12000 + k * 2000 - ~~(n / 10) * 2000
