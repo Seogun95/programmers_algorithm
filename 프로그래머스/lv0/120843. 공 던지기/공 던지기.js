694c4b1ddba2a94476fce950dad9bb4cd39f3cf1
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-22 17:19:50 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,4 +1,15 @@
 function solution(numbers, k) {
-    var answer = 0;
-    return answer;
+    //1. k번째 던졌을때 던져서 받는사람이 아니고 던진사람.
+    //2. 인덱스에서 2만큼 간다.
+    //3. 모든 시작은 1부터 시작한다.
+
+    let result = 1;
+    for (let i = 0; i < k - 1; i++) {
+        result += 2;
+        if (result > numbers.length) {
+            result -= numbers.length;
+        }
+    }
+    return result 
 }
+
