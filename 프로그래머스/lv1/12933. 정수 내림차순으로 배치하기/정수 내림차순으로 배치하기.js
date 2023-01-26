dcb57beb0881f617df3f0fc4529da4a155fe24cb
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-26 23:59:55 +0900

    Update 정수 내림차순으로 배치하기.js

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -2,3 +2,17 @@ function solution(n) {
     let toString = `${n}`;
     return parseInt([...toString].sort().reverse().join(''));
 }
+
+/*
+function solution(n) {
+    let toString = `${n}`.split('');
+    return parseInt(toString.sort((a, b)=> b-a).join(''));
+}
+*/
+
+/*
+function solution(n) {
+    let toString = `${n}`;
+    return parseInt([...toString].sort((a, b)=> b-a).join(''));
+}
+*/
