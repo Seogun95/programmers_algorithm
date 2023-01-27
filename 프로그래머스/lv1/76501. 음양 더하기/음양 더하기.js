dcdf6c29200988eebd530bf898c4e29890921ea6
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-27 13:49:38 +0900

    [level 1] Title: 음양 더하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,12 @@
+function solution(absolutes, signs) {
+    let result = 0;
+
+    for (let i = 0; i < absolutes.length; i++) {
+        if (signs[i] == false) {
+            result += absolutes[i] * -1;
+        } else {
+            result += absolutes[i];
+        }
+    }
+    return result;
+}
