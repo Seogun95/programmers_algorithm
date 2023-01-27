db5185e7f881aa9702cc5e21b307186c2da0915c
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-27 16:32:32 +0900

    [level 1] Title: 행렬의 덧셈, Time: 9.14 ms, Memory: 68.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,13 @@
+function solution(arr1, arr2) {
+    let answer = new Array();
+    let arr = new Array();
+
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr1[i].length; j++) {
+            arr.push(arr1[i][j] + arr2[i][j]);
+        }
+        answer.push(arr);
+        arr = new Array();
+    }
+    return answer
+}
