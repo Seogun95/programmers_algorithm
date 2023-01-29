5d06af593f7cfeb5bf6f4eb567f6e4b3fcf5ae10
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-30 03:46:05 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    let result = '';
+    let removeSpace = s.split(' ');
+    for (let i = 0; i < removeSpace.length; i++) {
+        for (let j = 0; j < removeSpace[i].length; j++) {
+            if (j % 2 === 0) result += removeSpace[i][j].toUpperCase();
+            else result += removeSpace[i][j].toLowerCase();
+        }
+        result += ' ';
+    }
+    console.log(result);
+    return result.slice(0, result.length - 1);
+}
