8a5f8263757b5ab4678a57c7b473eec07c3d7ada
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-30 14:40:44 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,37 +1,11 @@
-/*=======첫번째방법*/
 function solution(s) {
     let result = '';
     let removeSpace = s.split(' ');
     for (let i = 0; i < removeSpace.length; i++) {
-        for (let j = 0; j < removeSpace[i].length; j++) {
-            j % 2 === 0 ? result += removeSpace[i][j].toUpperCase() : result += removeSpace[i][j].toLowerCase();
+       for (let j = 0; j < removeSpace[i].length; j++) {
+          j % 2 === 0 ? result += removeSpace[i][j].toUpperCase() : result += removeSpace[i][j].toLowerCase();
         }
         result += ' ';
     }
     return result.slice(0, result.length - 1);
 }
-
-/*=======두번째방법*/
-function solution(s) {
-    let result = '';
-    let removeSpace = s.split(' ');
-    for (let i = 0; i < removeSpace.length; i++) {
-        for (let j = 0; j < removeSpace[i].length; j++) {
-            j % 2 === 0 ? result += removeSpace[i][j].toUpperCase() : result += removeSpace[i][j].toLowerCase();
-        }
-        result += ' ';
-    }
-    return result.slice(0, result.length - 1);
-}
-
-/*=======세번째방법*/
-function solution(s) {
-    let result = s
-        .split(' ')
-        .map((value) =>
-            [...value].map((v, i) => (i % 2 ? v.toLowerCase() : v.toUpperCase())).join('')
-        )
-        .join(' ');
-    return result
-}
-
