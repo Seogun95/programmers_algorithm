3fcc022bde2c0aec3c24592f5069928796533c96
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-30 14:50:30 +0900

    Update 자릿수 더하기.js

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,3 +1,4 @@
+/*=====첫번째 방법*/
 function solution(n) {
     let result = 0;
     let arr = `${n}`.split('').map((i) => Number(i));
@@ -8,3 +9,22 @@ function solution(n) {
     return result;
 }
 
+/*=====두번째 방법*/
+function solution(n) {
+    let result = 0;
+    let arr = `${n}`.split('').map(Number); 
+
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i];
+    }
+    return result;
+}
+
+/*=====세번째 방법*/
+function solution(n) {
+    var answer = 0;
+    //var a = n.toString().split('');
+    var a = [...n.toString()];
+    for (let i = 0; i < a.length; i++) answer += parseInt(a[i]);
+    return answer;
+}
