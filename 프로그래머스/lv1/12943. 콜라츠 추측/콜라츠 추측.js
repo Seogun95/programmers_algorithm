2ff0bf69aef1f8e532b3bc4d460efeb14ebe5dab
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-30 14:00:27 +0900

    [level 1] Title: 콜라츠 추측, Time: ., Memory: undefined -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,8 +1,9 @@
 function solution(num) {
     var answer = 0;
-    while (num !== 1) {
-        if (answer++ === 500) return -1;
-        num = num % 2 ? num * 3 + 1 : num /= 2
+
+    while (num !== 500 && num !== 1) {
+        num % 2 === 0 ? (num /= 2) : (num *= 3 + 1);
+        answer++;
     }
-    return answer
+    return answer == 1 ? answer : -1;
 }
