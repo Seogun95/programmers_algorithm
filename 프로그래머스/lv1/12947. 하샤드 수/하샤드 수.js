4c05ee986e9f203403506748664aedb133c6dff5
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-30 14:27:37 +0900

    Update 하샤드 수.js

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,6 +1,13 @@
+/*====첫번째 방법*/
 function solution(x) {
     let str = `${x}`.split('');
     let backToIntArr = str.map((x) => parseInt(x));
     let addAll = backToIntArr.reduce((a, b) => a + b);
     return x % addAll === 0 ;
+}
+
+/*====두번째 방법*/
+function solution(x) {
+    let str = `${x}`.split('').map((x) => parseInt(x)).reduce((a, b) => a + b) 
+    return x % str === 0;
 }
