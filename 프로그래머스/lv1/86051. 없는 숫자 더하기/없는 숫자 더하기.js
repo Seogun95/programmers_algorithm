1209c9c55c444511dffab8239d8c77f92f407fee
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-31 09:49:41 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 2.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,11 +1,7 @@
 function solution(numbers) {
     let answer = 0;
     for (let i = 0; i < 10; i++) {
-        if (numbers.includes(i)) {
-            answer += 0;
-        } else {
-            answer += i;
-        }
+        answer += numbers.includes(i) ? 0 : i
     }
     console.log(answer);
     return answer;
