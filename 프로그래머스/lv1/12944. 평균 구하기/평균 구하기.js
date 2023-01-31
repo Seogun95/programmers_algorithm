95cde95b027b4a3d1606a026517fbe8c639ad262
서근 <76584961+Seogun95@users.noreply.github.com>
2023-01-31 09:56:02 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,14 +1,3 @@
-/*====방법1*/
-function solution(arr) {
-    let answer = 0;
-    let sum = 0;
-
-    arr.forEach((_, i) => {
-        answer += arr[i];
-    });
-    return answer / arr.length;
-}
-/*====방법1*/
 function solution(arr) {
     return arr.reduce((a, b) => a + b) / arr.length;
 }
