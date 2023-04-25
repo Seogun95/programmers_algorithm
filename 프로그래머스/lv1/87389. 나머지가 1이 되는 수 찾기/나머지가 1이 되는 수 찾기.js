2dda44cb20472356d48b7c8fbd886f6e478cd417
Seogun95 <ksj083895@gmail.com>
2023-04-25 11:24:42 +0900

    feat: 87389 문제 풀이 추가

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -11,4 +11,18 @@ const solution = n => {
 
 const results = [a, b].map(n => solution(n));
 
-console.log(results);
+console.log('results ---->', results);
+
+// -----------
+
+const solution2 = (n, x = 1) => {
+  while (x++) {
+    if (n % x === 1) {
+      return x;
+    }
+  }
+};
+
+const results2 = [a, b].map(n => solution(n));
+
+console.log('results2 =====> ', results2);
